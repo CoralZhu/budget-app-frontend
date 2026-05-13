0b56e1f38662d59a97e32bc81aa57bd74492bf60
Coral Zhu <xiangcuz@andrew.cmu.edu>
2026-05-13 10:09:08 +0800

    feat: integrate Vant UI and verify setup

--- src/main.js
+++ src/main.js
@@ -6,9 +6,15 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import Vant from 'vant'
+import 'vant/lib/index.css'
+import '@vant/touch-emulator'
+
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+
+app.use(Vant)
 
 app.mount('#app')
